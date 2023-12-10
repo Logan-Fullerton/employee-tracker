3337e50220b897e5b3a7a8c22546ac44eaa156b6
unknown <fullertonlogan@gmail.com>
2023-12-10 14:58:20 -0800

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO employeeDepartment (department)
+VALUES ("a"),
+       ("b"),
+       ("c"),
+       ("d"),
+       ("e");
+
+INSERT INTO employeeRole (title, salary, department_id)
+VALUES (1, "a"),
+       (2, "b"),
+       (1, "c"),
+       (3, "d"),
+       (5, "e"),
+       (1, "f"),
+       (5, "g");
